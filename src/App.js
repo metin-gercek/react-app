738eb769a473c1b7981e4c1ccea862f0cd9d5610
metin-gercek <metin.gercek@edu.bc.fi>
2022-07-28 16:35:19 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,76 @@
+import React, { Component } from "react";
+import Navi from "./Navi";
+import CategoryList from "./CategoryList";
+import ProductList from "./ProductList";
+import { Container, Row, Col } from "reactstrap";
+
+export default class App extends Component {
+  state = { currentCategory: "", products: [], cart: [] };
+
+  componentDidMount() {
+    this.getProducts();
+  }
+
+  changeCategory = (category) => {
+    this.setState({ currentCategory: category.categoryName });
+
+    this.getProducts(category.id);
+  };
+
+  getProducts = (categoryId) => {
+    let url = "http://localhost:3000/products";
+
+    if (categoryId) {
+      url += "?categoryId=" + categoryId;
+    }
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => this.setState({ products: data }));
+  };
+
+  addToCart = (product) => {
+    let newCart = this.state.cart;
+    var addedItem = newCart.find(c=>c.product.id===product.id);
+    if(addedItem){
+      addedItem.quantity+=1;
+    }else{
+    newCart.push({ product: product, quantity: 1 });
+    this.setState({ cart: newCart });
+    console.log(newCart);
+    }
+    
+  };
+
+  render() {
+    let productInfo = { title: "Product List" };
+    let categoryInfo = { title: "Category List" };
+    return (
+      <div>
+        <Container>
+          <Navi
+            cart={this.state.cart}
+            
+          />
+
+          <Row>
+            <Col xs="3">
+              <CategoryList
+                currentCategory={this.state.currentCategory}
+                changeCategory={this.changeCategory}
+                info={categoryInfo}
+              />
+            </Col>
+            <Col xs="9">
+              <ProductList
+                addToCart={this.addToCart}
+                products={this.state.products}
+                info={productInfo}
+                currentCategory={this.state.currentCategory}
+              />
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
+}
